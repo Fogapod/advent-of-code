89c9c213519c62d45b950a56e7b160f3850544ca
Eugene <fogaprod@gmail.com>
2021-12-06 21:28:22 +0300

    unchecked_get everywhere

--- 2021/d4/src/main.rs
+++ 2021/d4/src/main.rs
@@ -7,7 +7,8 @@ const BOARD_COUNT: usize = 100;
 const RAW_NUMBERS_LENGTH: usize = 10 + 90 * 2 + 99;
 
 const RAW_BOARD_ROW_LENGTH: usize = BOARD_SIDE * 2 + BOARD_SIDE;
-const RAW_BOARD_LENGTH: usize = RAW_BOARD_ROW_LENGTH * BOARD_SIDE;
+// + 1 for extra newline
+const RAW_BOARD_LENGTH: usize = RAW_BOARD_ROW_LENGTH * BOARD_SIDE + 1;
 
 fn sum_unmarked(board: &[i64; BOARD_SIDE * BOARD_SIDE]) -> i64 {
     board.iter().filter(|&n| *n != -1).sum()
@@ -16,18 +17,22 @@ fn sum_unmarked(board: &[i64; BOARD_SIDE * BOARD_SIDE]) -> i64 {
 fn mark_number(board: &mut [i64; BOARD_SIDE * BOARD_SIDE], number: i64) -> bool {
     for i in 0..BOARD_SIDE {
         for j in 0..BOARD_SIDE {
-            if board[i * BOARD_SIDE + j] != number {
-                continue;
-            }
+            unsafe {
+                if *board.get_unchecked(i * BOARD_SIDE + j) != number {
+                    continue;
+                }
 
-            board[i * BOARD_SIDE + j] = -1;
+                *board.get_unchecked_mut(i * BOARD_SIDE + j) = -1;
+            }
 
             let mut score_x = 0;
             let mut score_y = 0;
 
             for k in 0..BOARD_SIDE {
-                score_x += board[i * BOARD_SIDE + k];
-                score_y += board[k * BOARD_SIDE + j];
+                unsafe {
+                    score_x += board.get_unchecked(i * BOARD_SIDE + k);
+                    score_y += board.get_unchecked(k * BOARD_SIDE + j);
+                }
             }
 
             return -score_x == BOARD_SIDE as i64 || -score_y == BOARD_SIDE as i64;
@@ -44,23 +49,24 @@ pub fn run1(input: &[u8]) -> i64 {
 
     let mut boards = [[0i64; BOARD_SIDE * BOARD_SIDE]; BOARD_COUNT];
 
-    (0..BOARD_COUNT).for_each(|board_num| {
-        for row_num in 0..BOARD_SIDE {
-            // + 1 to account for extra newline between boards
-            let start = board_num * (RAW_BOARD_LENGTH + 1) + row_num * RAW_BOARD_ROW_LENGTH;
-            let row = &boards_raw[start..start + RAW_BOARD_ROW_LENGTH];
-
+    for (board_num, raw_board) in boards_raw.chunks_exact(RAW_BOARD_LENGTH).enumerate() {
+        for (row_num, raw_row) in raw_board.chunks_exact(RAW_BOARD_ROW_LENGTH).enumerate() {
             for col_num in 0..BOARD_SIDE {
-                let n = row[(col_num * 3) + 1] - b'0'
-                    + if row[(col_num * 3)] != b' ' {
-                        (row[(col_num * 3)] - b'0') * 10
-                    } else {
-                        0
-                    };
-                boards[board_num][row_num * BOARD_SIDE + col_num] = n as i64;
+                unsafe {
+                    let n = raw_row.get_unchecked((col_num * 3) + 1) - b'0'
+                        + if *raw_row.get_unchecked(col_num * 3) != b' ' {
+                            (raw_row.get_unchecked(col_num * 3) - b'0') * 10
+                        } else {
+                            0
+                        };
+
+                    *boards
+                        .get_unchecked_mut(board_num)
+                        .get_unchecked_mut(row_num * BOARD_SIDE + col_num) = n as i64;
+                }
             }
         }
-    });
+    }
 
     let mut pointer = 0;
 
@@ -69,12 +75,13 @@ pub fn run1(input: &[u8]) -> i64 {
             break;
         }
 
-        let mut number = (numbers_raw[pointer] - b'0') as i64;
+        let mut number = unsafe { (numbers_raw.get_unchecked(pointer) - b'0') as i64 };
 
-        if numbers_raw[pointer + 1] == b',' {
+        if unsafe { *numbers_raw.get_unchecked(pointer + 1) } == b',' {
             pointer += 2;
         } else {
-            number = number * 10 + (numbers_raw[pointer + 1] - b'0') as i64;
+            number =
+                unsafe { number * 10 + (numbers_raw.get_unchecked(pointer + 1) - b'0') as i64 };
             pointer += 3;
         };
 
@@ -97,23 +104,25 @@ pub fn run2(input: &[u8]) -> i64 {
 
     let mut boards = [[0i64; BOARD_SIDE * BOARD_SIDE]; BOARD_COUNT];
 
-    (0..BOARD_COUNT).for_each(|board_num| {
-        for row_num in 0..BOARD_SIDE {
-            // + 1 to account for extra newline between boards
-            let start = board_num * (RAW_BOARD_LENGTH + 1) + row_num * RAW_BOARD_ROW_LENGTH;
-            let row = &boards_raw[start..start + RAW_BOARD_ROW_LENGTH];
-
+    // chunks instead of chunks_exact because last board does not have trailing \n\n
+    for (board_num, raw_board) in boards_raw.chunks(RAW_BOARD_LENGTH).enumerate() {
+        for (row_num, raw_row) in raw_board.chunks_exact(RAW_BOARD_ROW_LENGTH).enumerate() {
             for col_num in 0..BOARD_SIDE {
-                let n = row[(col_num * 3) + 1] - b'0'
-                    + if row[(col_num * 3)] != b' ' {
-                        (row[(col_num * 3)] - b'0') * 10
-                    } else {
-                        0
-                    };
-                boards[board_num][row_num * BOARD_SIDE + col_num] = n as i64;
+                unsafe {
+                    let n = raw_row.get_unchecked((col_num * 3) + 1) - b'0'
+                        + if *raw_row.get_unchecked(col_num * 3) != b' ' {
+                            (raw_row.get_unchecked(col_num * 3) - b'0') * 10
+                        } else {
+                            0
+                        };
+
+                    *boards
+                        .get_unchecked_mut(board_num)
+                        .get_unchecked_mut(row_num * BOARD_SIDE + col_num) = n as i64;
+                }
             }
         }
-    });
+    }
 
     let mut remaining_boards = BOARD_COUNT;
 
@@ -124,29 +133,32 @@ pub fn run2(input: &[u8]) -> i64 {
             break;
         }
 
-        let mut number = (numbers_raw[pointer] - b'0') as i64;
+        let mut number = unsafe { (numbers_raw.get_unchecked(pointer) - b'0') as i64 };
 
-        if numbers_raw[pointer + 1] == b',' {
+        if unsafe { *numbers_raw.get_unchecked(pointer + 1) } == b',' {
             pointer += 2;
         } else {
-            number = number * 10 + (numbers_raw[pointer + 1] - b'0') as i64;
+            number =
+                unsafe { number * 10 + (numbers_raw.get_unchecked(pointer + 1) - b'0') as i64 };
             pointer += 3;
         };
 
         let mut offset = 0;
 
         for i in 0..remaining_boards {
-            if mark_number(&mut boards[i], number) {
-                if remaining_boards == 1 {
-                    return sum_unmarked(&boards[i]) * number;
+            unsafe {
+                if mark_number(boards.get_unchecked_mut(i), number) {
+                    if remaining_boards == 1 {
+                        return sum_unmarked(boards.get_unchecked(i)) * number;
+                    }
+
+                    remaining_boards -= 1;
+
+                    continue;
                 }
 
-                remaining_boards -= 1;
-
-                continue;
+                *boards.get_unchecked_mut(offset) = *boards.get_unchecked(i);
             }
-
-            boards[offset] = boards[i];
 
             offset += 1;
         }
