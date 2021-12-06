58286347f5ab15c962e722a98d438766dd8a7f3b
Eugene <fogaprod@gmail.com>
2021-12-06 10:54:09 +0300

    Very efficient parsing with a lot of assumptions about data

--- 2021/d4/src/main.rs
+++ 2021/d4/src/main.rs
@@ -3,6 +3,12 @@ use std::fs;
 const BOARD_SIDE: usize = 5;
 const BOARD_COUNT: usize = 100;
 
+// 10 numbers (0 - 9) + 90 numbers (10 - 99) + 99 ','
+const RAW_NUMBERS_LENGTH: usize = 10 + 90 * 2 + 99;
+
+const RAW_BOARD_ROW_LENGTH: usize = BOARD_SIDE * 2 + BOARD_SIDE - 1 + 1;
+const RAW_BOARD_LENGTH: usize = RAW_BOARD_ROW_LENGTH * BOARD_SIDE;
+
 fn sum_unmarked(board: &[i64; BOARD_SIDE * BOARD_SIDE]) -> i64 {
     board.iter().filter(|&n| *n != -1).sum()
 }
@@ -31,24 +37,47 @@ fn mark_number(board: &mut [i64; BOARD_SIDE * BOARD_SIDE], number: i64) -> bool 
     false
 }
 
-pub fn run1(input: &str) -> i64 {
-    let (numbers, rest) = input.split_once("\n\n").unwrap();
+pub fn run1(input: &[u8]) -> i64 {
+    let numbers_raw = &input[..RAW_NUMBERS_LENGTH];
+    // skip 2 newlines
+    let boards_raw = &input[RAW_NUMBERS_LENGTH + 2..];
 
     let mut boards = [[0i64; BOARD_SIDE * BOARD_SIDE]; BOARD_COUNT];
 
-    for (board_num, board) in rest.split("\n\n").enumerate() {
-        for (row_num, line) in board.split('\n').enumerate() {
-            for (col_num, n) in line
-                .split_ascii_whitespace()
-                .map(|n| n.parse::<i64>().unwrap())
-                .enumerate()
-            {
-                boards[board_num][row_num * BOARD_SIDE + col_num] = n;
+    (0..BOARD_COUNT).for_each(|board_num| {
+        for row_num in 0..BOARD_SIDE {
+            // + 1 to account for extra newline between boards
+            let start = board_num * (RAW_BOARD_LENGTH + 1) + row_num * RAW_BOARD_ROW_LENGTH;
+            let row = &boards_raw[start..start + RAW_BOARD_ROW_LENGTH];
+
+            for col_num in 0..BOARD_SIDE {
+                let n = row[(col_num * 3) + 1] - b'0'
+                    + if row[(col_num * 3)] != b' ' {
+                        (row[(col_num * 3)] - b'0') * 10
+                    } else {
+                        0
+                    };
+                boards[board_num][row_num * BOARD_SIDE + col_num] = n as i64;
             }
         }
-    }
+    });
 
-    for number in numbers.split(',').map(|n| n.parse::<i64>().unwrap()) {
+    let mut pointer = 0;
+
+    loop {
+        if pointer >= RAW_NUMBERS_LENGTH {
+            break;
+        }
+
+        let mut number = (numbers_raw[pointer] - b'0') as i64;
+
+        if numbers_raw[pointer + 1] == b',' {
+            pointer += 2;
+        } else {
+            number = number * 10 + (numbers_raw[pointer + 1] - b'0') as i64;
+            pointer += 3;
+        };
+
         for board in &mut boards {
             if mark_number(board, number) {
                 return sum_unmarked(board) * number;
@@ -61,26 +90,49 @@ pub fn run1(input: &str) -> i64 {
 
 // this fails on some inputs, i dont know why
 #[allow(clippy::mut_range_bound)]
-pub fn run2(input: &str) -> i64 {
-    let (numbers, rest) = input.split_once("\n\n").unwrap();
+pub fn run2(input: &[u8]) -> i64 {
+    let numbers_raw = &input[..RAW_NUMBERS_LENGTH];
+    // skip 2 newlines
+    let boards_raw = &input[RAW_NUMBERS_LENGTH + 2..];
 
     let mut boards = [[0i64; BOARD_SIDE * BOARD_SIDE]; BOARD_COUNT];
 
-    for (board_num, board) in rest.split("\n\n").enumerate() {
-        for (row_num, line) in board.split('\n').enumerate() {
-            for (col_num, n) in line
-                .split_ascii_whitespace()
-                .map(|n| n.parse::<i64>().unwrap())
-                .enumerate()
-            {
-                boards[board_num][row_num * BOARD_SIDE + col_num] = n;
+    (0..BOARD_COUNT).for_each(|board_num| {
+        for row_num in 0..BOARD_SIDE {
+            // + 1 to account for extra newline between boards
+            let start = board_num * (RAW_BOARD_LENGTH + 1) + row_num * RAW_BOARD_ROW_LENGTH;
+            let row = &boards_raw[start..start + RAW_BOARD_ROW_LENGTH];
+
+            for col_num in 0..BOARD_SIDE {
+                let n = row[(col_num * 3) + 1] - b'0'
+                    + if row[(col_num * 3)] != b' ' {
+                        (row[(col_num * 3)] - b'0') * 10
+                    } else {
+                        0
+                    };
+                boards[board_num][row_num * BOARD_SIDE + col_num] = n as i64;
             }
         }
-    }
+    });
 
     let mut remaining_boards = BOARD_COUNT;
 
-    for number in numbers.split(',').map(|n| n.parse::<i64>().unwrap()) {
+    let mut pointer = 0;
+
+    loop {
+        if pointer >= RAW_NUMBERS_LENGTH {
+            break;
+        }
+
+        let mut number = (numbers_raw[pointer] - b'0') as i64;
+
+        if numbers_raw[pointer + 1] == b',' {
+            pointer += 2;
+        } else {
+            number = number * 10 + (numbers_raw[pointer + 1] - b'0') as i64;
+            pointer += 3;
+        };
+
         let mut offset = 0;
 
         for i in 0..remaining_boards {
@@ -104,7 +156,7 @@ pub fn run2(input: &str) -> i64 {
 }
 
 fn main() {
-    let input = &fs::read_to_string("input").unwrap();
+    let input = &fs::read("input").unwrap();
 
     println!("{}", run1(input));
     println!("{}", run2(input));
