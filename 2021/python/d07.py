a5361d20f6916e121f16a75b4734363f80d9ff05
Eugene <fogaprod@gmail.com>
2021-12-07 15:28:37 +0300

    trim newline at the end

--- 2021/python/d07.py
+++ 2021/python/d07.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 file = Path(__file__)
 
-with open(file.parent.parent / "input" / file.stem / "full2") as f:
+with open(file.parent.parent / "input" / file.stem / "full") as f:
     data = [int(i) for i in f.read().split(",")]
 
 
--- 2021/rust/d07/src/lib.rs
+++ 2021/rust/d07/src/lib.rs
@@ -5,6 +5,10 @@ use arrayvec::ArrayVec;
 const NUMBER_COUNT: usize = 1000;
 
 pub fn run1_brute(input: &[u8]) -> i64 {
+    // trim newline
+    let len = input.len();
+    let input = unsafe { &input[..len - (*input.get_unchecked(len - 1) == b'\n') as usize] };
+
     let mut sorted_numbers = input
         .split(|&byte| byte == b',')
         .map(|raw_number| {
@@ -12,7 +16,7 @@ pub fn run1_brute(input: &[u8]) -> i64 {
                 .iter()
                 .rev()
                 .enumerate()
-                .map(|(mut i, &byte)| {
+                .map(|(mut i, byte)| {
                     let mut byte = (byte - b'0') as i64;
 
                     while i > 0 {
@@ -50,6 +54,10 @@ pub fn run1_brute(input: &[u8]) -> i64 {
 }
 
 pub fn run1(input: &[u8]) -> i64 {
+    // trim newline
+    let len = input.len();
+    let input = unsafe { &input[..len - (*input.get_unchecked(len - 1) == b'\n') as usize] };
+
     let mut sorted_numbers = input
         .split(|&byte| byte == b',')
         .map(|raw_number| {
@@ -89,8 +97,12 @@ fn triangular_number(n: i64) -> i64 {
     n * (n + 1) / 2
 }
 
-// FIXME: produces wrong result for example
+// FIXME: produces wrong result for example, but works for everything else
 pub fn run2(input: &[u8]) -> i64 {
+    // trim newline
+    let len = input.len();
+    let input = unsafe { &input[..len - (*input.get_unchecked(len - 1) == b'\n') as usize] };
+
     let numbers = input
         .split(|&byte| byte == b',')
         .map(|raw_number| {
