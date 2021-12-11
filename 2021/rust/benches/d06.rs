672b71c6ddf2c0fd9add8814c6d2ec773006d3da
Eugene <fogaprod@gmail.com>
2021-12-11 18:24:10 +0300

    remove subtraction step

--- 2021/rust/benches/d06.rs
+++ 2021/rust/benches/d06.rs
@@ -4,7 +4,6 @@ fn bench(c: &mut Criterion) {
     let input = &std::fs::read("../input/d06/full").unwrap();
 
     c.bench_function("d06-1", |b| b.iter(|| d06::run1(input)));
-    c.bench_function("d06-1-simd", |b| b.iter(|| d06::run1_simd(input)));
     c.bench_function("d06-2", |b| b.iter(|| d06::run2(input)));
 }
 
